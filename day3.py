8bd07a814f7c9814c3658df298d7204f3e88339e
Lawrence Bensaid <lawrencebensaid@icloud.com>
2021-12-04 16:32:42 +0100

    Day 3

--- day3.py
+++ day3.py
@@ -1,3 +1,58 @@
+# --- Day 3: Binary Diagnostic ---
+
+# The submarine has been making some odd creaking noises, so you ask it to produce a diagnostic report just in case.
+
+# The diagnostic report (your puzzle input) consists of a list of binary numbers which, when decoded properly, can tell you many useful things about the conditions of the submarine. The first parameter to check is the power consumption.
+
+# You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and the epsilon rate). The power consumption can then be found by multiplying the gamma rate by the epsilon rate.
+
+# Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers in the diagnostic report. For example, given the following diagnostic report:
+
+# - 00100
+# - 11110
+# - 10110
+# - 10111
+# - 10101
+# - 01111
+# - 00111
+# - 11100
+# - 10000
+# - 11001
+# - 00010
+# - 01010
+# Considering only the first bit of each number, there are five 0 bits and seven 1 bits. Since the most common bit is 1, the first bit of the gamma rate is 1.
+
+# The most common second bit of the numbers in the diagnostic report is 0, so the second bit of the gamma rate is 0.
+
+# The most common value of the third, fourth, and fifth bits are 1, 1, and 0, respectively, and so the final three bits of the gamma rate are 110.
+
+# So, the gamma rate is the binary number 10110, or 22 in decimal.
+
+# The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used. So, the epsilon rate is 01001, or 9 in decimal. Multiplying the gamma rate (22) by the epsilon rate (9) produces the power consumption, 198.
+
+# Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together. What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)
+
+data = []
+with open('input/day3') as f:
+    data = f.read().splitlines()
+
+
+def decode(sequence, char='1'):
+    bit_counts = [0 for _ in range(len(sequence[0]))]
+    for data in sequence:
+        for bit in range(len(data)):
+            if data[bit] == char:
+                bit_counts[bit] += 1
+    binary = ''.join('1' if count > (len(sequence) / 2) else '0' for count in bit_counts)
+    return int(binary, 2)
+
+
+gamma_rate = decode(data)
+epsilon_rate = decode(data, char='0')
+
+print('\nGamma rate: {}\nEpsilon rate: {}\n\nPower consumption: {}\n'.format(gamma_rate, epsilon_rate, gamma_rate * epsilon_rate))
+
+
 # --- Part Two ---
 
 # Next, you should verify the life support rating, which can be determined by multiplying the oxygen generator rating by the CO2 scrubber rating.
@@ -29,16 +84,13 @@
 
 # Use the binary numbers in your diagnostic report to calculate the oxygen generator rating and CO2 scrubber rating, then multiply them together. What is the life support rating of the submarine? (Be sure to represent your answer in decimal, not binary.)
 
-data = []
-with open('input/day3') as f:
-    data = f.read().splitlines()
-
 def sequence_indices(sequence, bit_index, char='1'):
     indices = []
     for index, data in enumerate(sequence):
         if data[bit_index] == char:
             indices.append(index)
     return indices
+
 
 def decode_o2gen_rating(data, index=0):
     bits_1 = sequence_indices(data, bit_index=index)
@@ -50,6 +102,7 @@ def decode_o2gen_rating(data, index=0):
     else:
         return int(result[0], 2)
 
+
 def decode_co2scrubber_rating(data, index=0):
     bits_1 = sequence_indices(data, bit_index=index)
     bits_0 = sequence_indices(data, bit_index=index, char='0')
@@ -60,7 +113,8 @@ def decode_co2scrubber_rating(data, index=0):
     else:
         return int(result[0], 2)
 
+
 oxygen_generator_rating = decode_o2gen_rating(data)
 co2_scrubber_rating = decode_co2scrubber_rating(data)
 
-print('Oxygen generator rating: {}\nCO2 scrubber rating: {}\n\nLife support rating: {}'.format(oxygen_generator_rating, co2_scrubber_rating, oxygen_generator_rating * co2_scrubber_rating))
+print('\nOxygen generator rating: {}\nCO2 scrubber rating: {}\n\nLife support rating: {}\n'.format(oxygen_generator_rating, co2_scrubber_rating, oxygen_generator_rating * co2_scrubber_rating))
